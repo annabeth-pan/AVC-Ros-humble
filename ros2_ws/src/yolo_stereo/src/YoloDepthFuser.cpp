f91cd3e3518de8176933d5fbbae217e879862d8f
Bean <70710230+Storby42@users.noreply.github.com>
2026-04-09 21:08:34 -0400

    oopsie

--- ros2_ws/src/yolo_stereo/src/YoloDepthFuser.cpp
+++ ros2_ws/src/yolo_stereo/src/YoloDepthFuser.cpp
@@ -176,7 +176,6 @@ class YoloDepthFuser : public rclcpp::Node
         cv::imshow("apple", cropped);
         float medianermaktuallydisparity = medianMat(cropped, NBINS);
         RCLCPP_INFO(get_logger(), "Median disparity found to be %f", medianermaktuallydisparity);
-        RCLCPP_INFO(get_logger(), "Mean disparity found to be %f", cropped.mean(0));
         if (medianermaktuallydisparity < 0) {
           continue;
         }
